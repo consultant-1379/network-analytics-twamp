16c91dcf382a543625e58985ff715309a914d383
consultant <consultant-1379@tutamail.com>
2024-08-28 12:08:11 +0800

    first commit

---
+++ git_build.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+ 
+if [ "$2" == "" ]; then
+    	echo usage: $0 \<Branch\> \<RState\>
+    	exit -1
+else
+	versionProperties=install/version.properties
+	theDate=\#$(date +"%c")
+	module=$1
+	branch=$2
+	workspace=$3
+fi
+
+function getProductNumber {
+        product=`cat $PWD/build.cfg | grep $module | grep $branch | awk -F " " '{print $3}'`
+}
+
+function setRstate {
+        revision=`cat $PWD/build.cfg | grep $module | grep $branch | awk -F " " '{print $4}'`
+	
+	if git tag | grep $product-$revision; then
+        	rstate=`git tag | grep ${product}-${revision} | tail -1 | sed s/.*-// | perl -nle 'sub nxt{$_=shift;$l=length$_;sprintf"%0${l}d",++$_}print $1.nxt($2) if/^(.*?)(\d+$)/';`
+        else
+		ammendment_level=01
+	        rstate=$revision$ammendment_level
+	fi
+	mv twamp/build/feature-release.xml twamp/build/feature-release.${rstate}.xml
+		echo "Building rstate:$rstate"
+}
+function getRelease {
+	        release=`cat $PWD/build.cfg | grep $module | grep $branch | awk -F " " '{print $5}'`
+}
+
+
+function Arm104nexusDeploy {
+	RepoURL=https://arm104-eiffel004.lmera.ericsson.se:8443/nexus/content/repositories/assure-releases 
+
+	GroupId=com.ericsson.eniq.netanserver.features
+	ArtifactId=$module
+	zipName=Ericsson-TWAMP-Analysis
+	
+	
+	echo "****"	
+	echo "Deploying the zip /$zipName-$release.zip as ${ArtifactId}${rstate}.zip to Nexus...."
+        mv target/$zipName-$release.zip target/${ArtifactId}.zip
+	echo "****"	
+
+  	mvn deploy:deploy-file \
+	        	-Durl=${RepoURL} \
+		        -DrepositoryId=assure-releases \
+		        -DgroupId=${GroupId} \
+		        -Dversion=${rstate} \
+		        -DartifactId=${ArtifactId} \
+		        -Dfile=target/${ArtifactId}.zip
+		      
+}
+
+getProductNumber
+setRstate
+getRelease
+git checkout $branch
+git pull origin $branch
+
+#add maven command here
+mvn package
+
+Arm104nexusDeploy
+
+rsp=$?
+
+if [ $rsp == 0 ]; then
+
+  git tag $product-$rstate
+  git push --tag origin $branch
+
+fi
+
+exit $rsp
